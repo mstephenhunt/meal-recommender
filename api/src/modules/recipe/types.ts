cb843e2fde299cb7e4ee7f00606c377235d88bd5
Matt Hunt <mstephenhunt@gmail.com>
2023-08-14 14:45:41 -0400

    save ingredients

---
+++ api/src/modules/recipe/types.ts
@@ -0,0 +1,35 @@
+export type Ingredient = {
+  id: number;
+  name: string;
+};
+
+export type IngredientInput = {
+  name: string;
+};
+
+export type RecipeIngredient = {
+  id: number;
+  recipeId: number;
+  ingredient: Ingredient;
+  quantity: number;
+  unit: string;
+};
+
+export type RecipeIngredientInput = {
+  ingredient: IngredientInput;
+  quantity: number;
+  unit: string;
+};
+
+export type Recipe = {
+  id: number;
+  name: string;
+  instructions: string;
+  recipeIngredients: RecipeIngredient[];
+};
+
+export type RecipeInput = {
+  name: string;
+  instructions: string;
+  recipeIngredients: RecipeIngredientInput[];
+};
