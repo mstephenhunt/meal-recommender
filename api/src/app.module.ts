3a8af399d9f5d7924442ea568d93676ab2949bf1
Matt Hunt <mstephenhunt@gmail.com>
2023-08-15 07:23:43 -0400

    test setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { RecipeModule } from './modules/recipe/recipe.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, RecipeModule],
+  imports: [UserModule, RecipeModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
