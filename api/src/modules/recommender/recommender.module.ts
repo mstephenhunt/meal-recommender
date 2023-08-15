27da2c065730650d5321fc6a01b4ce6265708b9f
Matt Hunt <mstephenhunt@gmail.com>
2023-08-15 13:45:44 -0400

    use http

--- api/src/modules/recommender/recommender.module.ts
+++ api/src/modules/recommender/recommender.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OpenaiService } from './services/openai.service';
 import { OpenaiTestControllerController } from './controllers/openai-test-controller.controller';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, HttpModule],
   controllers: [OpenaiTestControllerController],
   providers: [OpenaiService],
 })
