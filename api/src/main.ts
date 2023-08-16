5c4e78eb2e46767e35c88faab489252290d0e557
Matt Hunt <mstephenhunt@gmail.com>
2023-08-16 08:10:53 -0400

    logging

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(3000);
 }
 bootstrap();
--- api/src/modules/recommender/controllers/recommender.controller.ts
+++ api/src/modules/recommender/controllers/recommender.controller.ts
@@ -1,14 +1,22 @@
 import { Get, Controller, UseGuards, Body } from '@nestjs/common';
 import { RecommenderService } from '../services/recommender.service';
 import { APIGuard } from '../../auth/guards/api.guard';
+import { Logger } from 'nestjs-pino';
 
 @Controller('recommender')
 @UseGuards(APIGuard)
 export class RecommenderController {
-  constructor(private readonly recommenderService: RecommenderService) {}
+  constructor(
+    private readonly recommenderService: RecommenderService,
+    private readonly logger: Logger,
+  ) {}
 
   @Get('/generate-recipe')
   public async test(@Body() input: { recipeName: string }): Promise<string> {
+    this.logger.log('Generating recipe', {
+      recipeName: input.recipeName,
+    });
+
     const response = await this.recommenderService.requestRecipe({
       recipeName: input.recipeName,
     });
