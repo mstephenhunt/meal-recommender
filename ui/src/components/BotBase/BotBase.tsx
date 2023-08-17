3448d22467136db6a1793dfdd7d8a028e7f0707d
Matt Hunt <mstephenhunt@gmail.com>
2023-08-17 09:46:31 -0400

    as props

--- ui/src/components/BotBase/BotBase.tsx
+++ ui/src/components/BotBase/BotBase.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Container } from "@mui/material";
 import bot from "../../images/reduced.png";
-import Typography from '@mui/material/Typography';
 
-export default function BotBase() {
+type BotBaseProps = {
+  speechText: string;
+}
+
+export default function BotBase({ speechText }: BotBaseProps) {
   return (
     <Container maxWidth="xs">
       <div style={{ textAlign: "center", paddingTop: "20px", position: "relative" }}>
-        <img src={bot} alt="bot" />
+        <img src={bot} height={"300px"} width="350px" alt="bot" />
         <div
           style={{
             position: "absolute",
             top: "50%",
             left: "50%",
-            transform: "translate(-93%, -166%)",
-            width: "130px",
-            height: "60px",
+            transform: "translate(-95%, -192%)",
+            width: "175px",
+            height: "65px",
             textAlign: "center",
             color: "black",
             background: "#ffffff00",
-            fontSize: "14px",
+            fontSize: "18px",
           }}
         >
-          Welcome! Anything you'd like me to know?
+          {speechText}
         </div>
       </div>
     </Container>
--- ui/src/components/MainMenu/MainMenuPage.tsx
+++ ui/src/components/MainMenu/MainMenuPage.tsx
@@ -4,7 +4,7 @@ import BotBase from "../BotBase/BotBase";
 export default function MainMenuPage() {
   return (
     <div>
-      <BotBase />
+      <BotBase speechText="Welcome! Anything you'd like me to know?"/>
     </div>
   );
 }
