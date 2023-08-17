c4d91abd175ff86027ff4f20ba7c05507591e4f5
Matt Hunt <mstephenhunt@gmail.com>
2023-08-17 10:13:05 -0400

    buttons

--- ui/src/components/MainMenu/MainMenuPage.tsx
+++ ui/src/components/MainMenu/MainMenuPage.tsx
@@ -1,10 +1,57 @@
 import React from "react";
 import BotBase from "../BotBase/BotBase";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 export default function MainMenuPage() {
   return (
-    <div>
-      <BotBase speechText="Welcome! Anything you'd like me to know?"/>
-    </div>
+    <Container
+      maxWidth="xs"
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <BotBase 
+        speechText="Welcome! Anything you'd like me to know?"
+      />
+      <Button
+        variant="contained"
+        sx={{
+          height: "70px",
+          width: "100%",
+          marginTop: "20px",
+          backgroundColor: "#53b94f",
+          fontWeight: "bold",
+        }}
+      >
+        Suggest Recipes
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          height: "70px",
+          width: "100%",
+          marginTop: "20px",
+          backgroundColor: "#53b94f",
+          fontWeight: "bold",
+        }}
+      >
+        Add Dietary Restrictions
+      </Button>
+      <Button
+        variant="contained"
+        sx={{
+          height: "70px",
+          width: "100%",
+          marginTop: "20px",
+          backgroundColor: "#53b94f",
+          fontWeight: "bold",
+        }}
+      >
+        Add Previous Meals
+      </Button>
+    </Container>
   );
 }
