bc1f195ad546edf06173d0ae1bf821436f99d1d4
Matt Hunt <mstephenhunt@gmail.com>
2023-08-17 10:48:41 -0400

    formatting

--- ui/src/components/MainMenu/MainMenuPage.tsx
+++ ui/src/components/MainMenu/MainMenuPage.tsx
@@ -2,56 +2,66 @@ import React from "react";
 import BotBase from "../BotBase/BotBase";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function MainMenuPage() {
   return (
-    <Container
-      maxWidth="xs"
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <BotBase 
-        speechText="Welcome! Anything you'd like me to know?"
-      />
-      <Button
-        variant="contained"
-        sx={{
-          height: "70px",
-          width: "100%",
-          marginTop: "20px",
-          backgroundColor: "#53b94f",
-          fontWeight: "bold",
+    <div>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <div></div> {/* Empty div to push the Log Out button to the right */}
+          <Button color="inherit">Log Out</Button>
+        </Toolbar>
+      </AppBar>
+      <Container
+        maxWidth="xs"
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        Suggest Recipes
-      </Button>
-      <Button
-        variant="contained"
-        sx={{
-          height: "70px",
-          width: "100%",
-          marginTop: "20px",
-          backgroundColor: "#53b94f",
-          fontWeight: "bold",
-        }}
-      >
-        Add Dietary Restrictions
-      </Button>
-      <Button
-        variant="contained"
-        sx={{
-          height: "70px",
-          width: "100%",
-          marginTop: "20px",
-          backgroundColor: "#53b94f",
-          fontWeight: "bold",
-        }}
-      >
-        Add Previous Meals
-      </Button>
-    </Container>
+        <BotBase
+          speechText="Welcome! Anything you'd like me to know?"
+        />
+        <Button
+          variant="contained"
+          sx={{
+            height: "70px",
+            width: "100%",
+            marginTop: "20px",
+            backgroundColor: "#53b94f",
+            fontWeight: "bold",
+          }}
+        >
+          Suggest Recipes
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            height: "70px",
+            width: "100%",
+            marginTop: "20px",
+            backgroundColor: "#53b94f",
+            fontWeight: "bold",
+          }}
+        >
+          Add Dietary Restrictions
+        </Button>
+        <Button
+          variant="contained"
+          sx={{
+            height: "70px",
+            width: "100%",
+            marginTop: "20px",
+            backgroundColor: "#53b94f",
+            fontWeight: "bold",
+          }}
+        >
+          Add Previous Meals
+        </Button>
+      </Container>
+    </div>
   );
 }
