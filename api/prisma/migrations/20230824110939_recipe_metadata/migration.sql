a04f9f5384c614dbf1cbcb8308b2af12c03c65d5
Matt Hunt <mstephenhunt@gmail.com>
2023-08-24 09:45:56 -0400

    remove unused model

--- api/prisma/migrations/20230824110939_recipe_metadata/migration.sql
+++ api/prisma/migrations/20230824110939_recipe_metadata/migration.sql
@@ -11,14 +11,3 @@ CREATE TABLE "recipe_dietary_restrictions" (
 
     CONSTRAINT "recipe_dietary_restrictions_pkey" PRIMARY KEY ("id")
 );
-
--- CreateTable
-CREATE TABLE "recipe_user_generated" (
-    "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "recipe_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
-
-    CONSTRAINT "recipe_user_generated_pkey" PRIMARY KEY ("id")
-);
