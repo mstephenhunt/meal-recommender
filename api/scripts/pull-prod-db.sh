3001fbf97e8e209f7aa714a36dc19cc3081c3289
Matt Hunt <mstephenhunt@gmail.com>
2023-08-31 06:45:13 -0400

    perms

--- api/scripts/pull-prod-db.sh
+++ api/scripts/pull-prod-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Connection strings
-prod_connection="postgresql://your-production-username:your-production-password@your-production-host:your-production-port/your-production-dbname"
+prod_connection="postgresql://mealrecommenderprod_user:Hos6Cae3HVYxyPRQUwtB0oo5loI2NuME@dpg-cjghv1b37aks73cloe2g-a.ohio-postgres.render.com/mealrecommenderprod"
 local_connection="postgresql://postgres@localhost/meal_recommender"
 
 # Dump the production database to a file
