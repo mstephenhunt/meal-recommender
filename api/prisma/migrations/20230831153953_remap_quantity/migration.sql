3b03d69dc2ee9fe00757298691f6656b2b844d0a
Matt Hunt <mstephenhunt@gmail.com>
2023-08-31 11:41:51 -0400

    #48 recipe zero amts (#50)
    * as is
    
    * perms
    
    * prod restore script
    
    * prod restore script
    
    * remove dump data
    
    * goodbye bad data

---
+++ api/prisma/migrations/20230831153953_remap_quantity/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "recipe_ingredients" ALTER COLUMN "quantity" SET DATA TYPE DOUBLE PRECISION;
---
+++ api/pull-prod-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Load environment variables from .env
+export $(cat .env | xargs)
+
+# Connection strings
+prod_connection="${PROD_DATABASE_URL}"
+local_connection="${DATABASE_URL}"
+
+# Dump the production database to a file
+dump_file="production_dump.sql"
+pg_dump "$prod_connection" --file="$dump_file"
+
+# Drop all tables in the local database
+psql "$local_connection" --command="DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+
+# # Restore the dump to the local database
+psql "$local_connection" --file="$dump_file"
+
+# # Remove the dump file
+rm "$dump_file"
+
+echo "Database cloned from production to local."
