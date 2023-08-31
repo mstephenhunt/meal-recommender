f3d7c4e10b64506ad9c5d12b5f99a338d32755ce
Matt Hunt <mstephenhunt@gmail.com>
2023-08-31 11:41:25 -0400

    goodbye bad data

---
+++ api/prisma/migrations/20230831153603_drop_bad_data/migration.sql
@@ -0,0 +1,6 @@
+-- This is to clean up all the junk data. Easier to just dump and start over.
+DELETE FROM ingredients;
+DELETE FROM recipe_dietary_restrictions;
+DELETE FROM recipe_ingredients;
+DELETE FROM recipes;
+DELETE FROM user_recipes;
