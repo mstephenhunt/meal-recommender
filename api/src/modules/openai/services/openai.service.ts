2302282b966c4769b0c8a220f4fadf2dfc69a0b3
Matt Hunt <mstephenhunt@gmail.com>
2023-09-06 11:54:51 -0400

    actually pass restrictions

--- api/src/modules/openai/services/openai.service.ts
+++ api/src/modules/openai/services/openai.service.ts
@@ -72,10 +72,17 @@ export class OpenaiService {
 
   public async requestRecipe(input: {
     recipeName: string;
+    dietaryRestrictions?: string[];
   }): Promise<OpenAIMeal> {
+    const dietaryRestrictionsMessage = input.dietaryRestrictions
+      ? `The recipe should fit the dietary restrictions: ${input.dietaryRestrictions.join(
+          ', ',
+        )}.`
+      : undefined;
+
     const initializeMessage = {
       role: OpenAIRole.SYSTEM,
-      content: `Please provide a recipe for ${input.recipeName}. ${this.responseFormat}`,
+      content: `Please provide a recipe for ${input.recipeName}. ${dietaryRestrictionsMessage} ${this.responseFormat}`,
     };
 
     this.logger.log('Sending message to OpenAI', {
@@ -144,28 +151,5 @@ export class OpenaiService {
     await this.requestCacheService.cacheAxiosRequestResponse(response);
 
     return response.data.choices[0].message.content;
-  }
-
-  public async testChat(): Promise<string> {
-    const response = await this.httpService
-      .post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          model: 'gpt-3.5-turbo',
-          messages: [{ role: 'user', content: 'Say this is a test!' }],
-          temperature: 0.7,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.apiKey}`,
-          },
-        },
-      )
-      .toPromise();
-
-    await this.requestCacheService.cacheAxiosRequestResponse(response);
-
-    return response.data;
   }
 }
