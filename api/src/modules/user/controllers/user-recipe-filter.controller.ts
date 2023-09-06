5ae1806c44cedf56322ffa6133af91fcefa380ec
Matt Hunt <mstephenhunt@gmail.com>
2023-09-06 07:25:38 -0400

    endpoints and filters
    :

--- api/src/modules/user/controllers/user-recipe-filter.controller.ts
+++ api/src/modules/user/controllers/user-recipe-filter.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Delete, UseGuards, Body } from '@nestjs/common';
 import { UserGuard } from '../../auth/guards/user.guard';
 import { UserRecipeFiltersService } from '../services/user-recipe-filters.service';
 import { IngredientFilterEntity } from '../../recipe/entities/ingredient-filter.entity';
+import { DietFilterEntity } from '../../recipe/entities/diet-filter.entity';
+import { AllergenFilterEntity } from '../../recipe/entities/allergen-filter.entity';
 
 @Controller('/api/user/recipe-filters')
 @UseGuards(UserGuard)
@@ -42,6 +44,72 @@ export class UserRecipeFiltersController {
 
     await this.userRecipeFiltersService.deleteUserIngredient({
       ingredientId,
+    });
+  }
+
+  @Get('diets')
+  public async getUserDiets(): Promise<DietFilterEntity[]> {
+    const userFilterDiets = await this.userRecipeFiltersService.getUserDiets();
+
+    return userFilterDiets.map((diet) => new DietFilterEntity(diet));
+  }
+
+  @Post('diets')
+  public async addUserDiet(
+    @Body() input: { dietName: string },
+  ): Promise<DietFilterEntity> {
+    const { dietName } = input;
+
+    const dietFilter = await this.userRecipeFiltersService.upsertUserDiet({
+      dietName,
+    });
+
+    return new DietFilterEntity(dietFilter);
+  }
+
+  @Delete('diets')
+  public async deleteUserDiet(
+    @Body() input: { dietId: number },
+  ): Promise<void> {
+    const { dietId } = input;
+
+    await this.userRecipeFiltersService.deleteUserDiet({
+      dietId,
+    });
+  }
+
+  @Get('allergens')
+  public async getUserAllergens(): Promise<AllergenFilterEntity[]> {
+    const userFilterAllergens =
+      await this.userRecipeFiltersService.getUserAllergens();
+
+    return userFilterAllergens.map(
+      (allergen) => new AllergenFilterEntity(allergen),
+    );
+  }
+
+  @Post('allergens')
+  public async addUserAllergen(
+    @Body() input: { allergenName: string },
+  ): Promise<AllergenFilterEntity> {
+    const { allergenName } = input;
+
+    const allergenFilter =
+      await this.userRecipeFiltersService.upsertUserAllergen({
+        allergenName,
+      });
+
+    return new AllergenFilterEntity(allergenFilter);
+  }
+
+  @Delete('allergens')
+  public async deleteUserAllergen(
+    @Body() input: { allergenId: number },
+  ): Promise<void> {
+    const { allergenId } = input;
+
+    await this.userRecipeFiltersService.deleteUserAllergen({
+      allergenId,
     });
   }
 }
--- api/src/modules/user/services/user-recipe-filters.service.ts
+++ api/src/modules/user/services/user-recipe-filters.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { RecipeFilterService } from '../../recipe/services/recipe-filter.service';
-import { Ingredient } from '../../recipe/types';
+import { Ingredient, Diet, Allergen } from '../../recipe/types';
 import { PrismaService } from '../../db/services/prisma.service';
 import { UserContextService } from './user-context.service';
 import { Prisma } from '@prisma/client';
@@ -103,5 +103,181 @@ export class UserRecipeFiltersService {
     });
 
     return ingredient;
+  }
+
+  public async getUserDiets(): Promise<Diet[]> {
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Getting user diets', {
+      userId,
+    });
+
+    const diets = await this.prismaService.$queryRaw<Diet[]>(
+      Prisma.sql`
+        SELECT
+          d.id,
+          d.name,
+          d.display_name as "displayName"
+        FROM
+          user_diets ud
+          JOIN diets d ON ud.diet_id = d.id
+        WHERE
+          ud.user_id = ${userId};
+      `,
+    );
+
+    return diets;
+  }
+
+  public async deleteUserDiet(input: { dietId: number }): Promise<void> {
+    const { dietId } = input;
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Deleting user diet', {
+      dietId,
+      userId,
+    });
+
+    await this.prismaService.$queryRaw<Diet[]>(
+      Prisma.sql`
+        DELETE FROM
+          user_diets
+        WHERE
+          user_id = ${userId}
+          AND diet_id = ${dietId};
+      `,
+    );
+  }
+
+  public async upsertUserDiet(input: { dietName: string }): Promise<Diet> {
+    const { dietName } = input;
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Upserting user diet', {
+      dietName,
+      userId,
+    });
+
+    // Start a transaction
+    let diet;
+    await this.prismaService.$transaction(async (prisma) => {
+      diet = await this.recipeFilterService.upsertDiet({
+        dietInput: {
+          name: dietName,
+        },
+        prismaTransaction: prisma,
+      });
+
+      await prisma.usersDiets.upsert({
+        where: {
+          userId_dietId: {
+            userId,
+            dietId: diet.id,
+          },
+        },
+        update: {}, // No-op, thing already exists
+        create: {
+          userId,
+          dietId: diet.id,
+        },
+      });
+    });
+
+    this.logger.log('Added diet to user', {
+      diet,
+      userId: this.userContextService.userId,
+    });
+
+    return diet;
+  }
+
+  public async getUserAllergens(): Promise<Allergen[]> {
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Getting user allergens', {
+      userId,
+    });
+
+    const allergens = await this.prismaService.$queryRaw<Allergen[]>(
+      Prisma.sql`
+        SELECT
+          a.id,
+          a.name,
+          a.display_name as "displayName"
+        FROM
+          user_allergens ua
+          JOIN allergens a ON ua.allergen_id = a.id
+        WHERE
+          ua.user_id = ${userId};
+      `,
+    );
+
+    return allergens;
+  }
+
+  public async deleteUserAllergen(input: {
+    allergenId: number;
+  }): Promise<void> {
+    const { allergenId } = input;
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Deleting user allergen', {
+      allergenId,
+      userId,
+    });
+
+    await this.prismaService.$queryRaw<Allergen[]>(
+      Prisma.sql`
+        DELETE FROM
+          user_allergens
+        WHERE
+          user_id = ${userId}
+          AND allergen_id = ${allergenId};
+      `,
+    );
+  }
+
+  public async upsertUserAllergen(input: {
+    allergenName: string;
+  }): Promise<Allergen> {
+    const { allergenName } = input;
+    const userId = this.userContextService.userId;
+
+    this.logger.log('Upserting user allergen', {
+      allergenName,
+      userId,
+    });
+
+    // Start a transaction
+    let allergen;
+    await this.prismaService.$transaction(async (prisma) => {
+      allergen = await this.recipeFilterService.upsertAllergen({
+        allergenInput: {
+          name: allergenName,
+        },
+        prismaTransaction: prisma,
+      });
+
+      await prisma.usersAllergens.upsert({
+        where: {
+          userId_allergenId: {
+            userId,
+            allergenId: allergen.id,
+          },
+        },
+        update: {}, // No-op, thing already exists
+        create: {
+          userId,
+          allergenId: allergen.id,
+        },
+      });
+    });
+
+    this.logger.log('Added allergen to user', {
+      allergen,
+      userId: this.userContextService.userId,
+    });
+
+    return allergen;
   }
 }
