d04439c9dbc30059ec1c40d6e65575ab9f9c4729
Matt Hunt <mstephenhunt@gmail.com>
2023-09-23 06:40:56 -0400

    tweak message

--- api/src/modules/user/services/get-filtered-recipe.service.ts
+++ api/src/modules/user/services/get-filtered-recipe.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { RecipeService } from '../../recipe/services/recipe.service';
 import { UserRecipeFiltersService } from './user-recipe-filters.service';
 import { Recipe } from '../../recipe/types';
+import { PrismaService } from '../../db/services/prisma.service';
+import { UserContextService } from './user-context.service';
 
 @Injectable()
 export class GetFilteredRecipeService {
   constructor(
     private readonly recipeService: RecipeService,
     private readonly userRecipeFiltersService: UserRecipeFiltersService,
+    private readonly prismaService: PrismaService,
+    private readonly userContextService: UserContextService,
   ) {}
 
   public async getFilteredRecipeNames(): Promise<string[]> {
@@ -35,11 +39,22 @@ export class GetFilteredRecipeService {
       await this.userRecipeFiltersService.getUserAllergens();
     const filterDiets = await this.userRecipeFiltersService.getUserDiets();
 
-    return this.recipeService.requestFilteredRecipe({
+    const recipe = await this.recipeService.requestFilteredRecipe({
       recipeName,
       ingredients: filterIngredients,
       allergens: filterAllergens,
       diets: filterDiets,
     });
+
+    // Associate this recipe to this user
+    const userId = this.userContextService.userId;
+    await this.prismaService.userRecipe.create({
+      data: {
+        userId,
+        recipeId: recipe.id,
+      },
+    });
+
+    return recipe;
   }
 }
