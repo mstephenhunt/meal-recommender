a8f849736caebb0d66ef43dc0d7660c8b014f339
Matt Hunt <mstephenhunt@gmail.com>
2023-09-23 06:41:18 -0400

    tweak

--- ui/src/components/FilteredRecipe/FilteredRecipe.tsx
+++ ui/src/components/FilteredRecipe/FilteredRecipe.tsx
@@ -93,7 +93,7 @@ export default function FilteredRecipe() {
         <Button 
           variant="contained"
           sx={{ textTransform: "none", marginTop: '10px', marginBottom: '10px' }}
-          onClick={() => navigate('/home')}
+          onClick={() => navigate('/build-recipe')}
         >
           Back
         </Button>
